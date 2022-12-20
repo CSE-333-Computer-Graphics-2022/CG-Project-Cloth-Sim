479c232fc4eae9274ea52e3ae94813509f63adea
Chaitanya20044 <88967930+Chaitanya20044@users.noreply.github.com>
2022-12-20 10:38:53 +0530

    Add files via upload

---
+++ build/CMakeFiles/ClothSimProject.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "../ClothSimProject"
+  "../ClothSimProject.pdb"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui.cpp.o.d"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/depends/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/ClothSimProject.dir/src/main.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/src/main.cpp.o.d"
+  "CMakeFiles/ClothSimProject.dir/src/utils.cpp.o"
+  "CMakeFiles/ClothSimProject.dir/src/utils.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClothSimProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
